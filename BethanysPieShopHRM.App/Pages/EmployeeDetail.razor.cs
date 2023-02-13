b0f104c8cbfc6a4fd0a2805572f4358fcff41955
leozin777 <leo.cardoso@rede.ulbra.br>
2023-02-12 21:50:12 -0300

    navigation manager add

---
+++ BethanysPieShopHRM.App/Pages/EmployeeDetail.razor.cs
@@ -0,0 +1,28 @@
+﻿using BethanysPieShopHRM.App.Models;
+using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.App.Pages
+{
+    public partial class EmployeeDetail
+    {
+        [Parameter]
+        public string EmployeeId { get; set; }
+
+        public Employee? Employee { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            Employee = MockDataService.Employees.FirstOrDefault(e => e.EmployeeId == int.Parse(EmployeeId));
+            return base.OnInitializedAsync();
+        }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = default!;
+
+        public void NavigateToDetails(Employee selectedEmployee)
+        {
+            NavigationManager.NavigateTo($"/employeedetail/{selectedEmployee.EmployeeId}");
+        }
+    }
+}
